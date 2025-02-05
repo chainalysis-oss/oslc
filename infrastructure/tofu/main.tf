6c18dbac48e4cb7f147316b5732057781b4c8426
Mads R. Havmand <mads@v42.dk>
2025-02-05 11:14:37 +0100

    infra: deprovision infra

--- infrastructure/tofu/main.tf
+++ infrastructure/tofu/main.tf
@@ -6,8 +6,16 @@ terraform {
       source  = "spacelift-io/spacelift"
       version = "~> 1.19.0"
     }
+    ovh = {
+      source  = "ovh/ovh"
+      version = "1.5.0"
+    }
   }
 }
 
 provider "spacelift" {}
+
+provider "ovh" {
+  endpoint = "ovh-eu"
+}
 
